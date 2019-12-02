8fb1ebd700d70c52a30bcc21dc9efb2e16830839
rayx <ray_scqhg@163.com>
2019-12-02 16:24:14 +0800

    update

--- MaxEnt/__init__.py
+++ MaxEnt/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding:utf-8 -*-
 # @Time    : 2019/12/2 15:59
 # @Author  : Ray.X
+
--- MaxEnt/maxent.py
+++ MaxEnt/maxent.py
@@ -1,3 +1,9 @@
 # -*- coding:utf-8 -*-
 # @Time    : 2019/12/2 16:14
 # @Author  : Ray.X
+"""
+   GIS求解以了解最大熵的原理
+"""
+
+
+class MaxEnt:
